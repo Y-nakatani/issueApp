2e6dcfe2302b447bd4c1a854fa8cdb2d550db821
Y-nakatani <yuki.nakatani@supership.jp>
2018-05-25 17:54:50 +0900

    html出力成功

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,3 +4,10 @@ class ApplicationController < ActionController::Base
    render html: "hello, world!"
  end
 end
+
+#class mainpage < ActionController::Base
+#  protect_from_forgery with: :exception
+#  def index
+#    render html: "イエエエ"
+#  end
+#end
